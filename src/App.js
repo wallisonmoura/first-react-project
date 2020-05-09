3732af30f9053e104573e696b4fc9f705a01b439
wallisonmoura <wallison_cedaspy@hotmail.com>
2020-05-08 21:38:58 -0300

    Estilizando página Main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Routes />;
+      <GlobalStyle />
+    </>
   );
 }
 
